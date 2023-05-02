822616b24132f7d9a8c43f165ec77c71c3a88917
Kazim Kayhan <mail4kazim.kayhan@gmail.com>
2023-05-02 16:03:20 +0430

    hash table interview example for efficiency

--- hashTable.js
+++ hashTable.js
@@ -1,78 +1,72 @@
 class HashTable {
-    constructor(size = 7) {
-        this.dataMap = new Array(size);
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
-   
-    _hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+    return hash;
+  }
+
+  printTable() {
+    for (let i = 0; i < this.dataMap.length; i++) {
+      console.log(i, ": ", this.dataMap[i]);
+    }
+  }
+
+  set(key, value) { 
+    let index = this._hash(key);
+    if (!this.dataMap[index]) this.dataMap[index] = [];
+
+    this.dataMap[index].push([key, value]);
+    return this;
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
         }
-        return hash;
+      }
     }
+    return undefined;
+  }
 
-    printTable() {
-        for (let i = 0; i < this.dataMap.length; i++) {
-            console.log(i, ": ", this.dataMap[i]);
+  keys() {
+    let allKeys = [];
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          allKeys.push(this.dataMap[i][j][0]);
         }
+      }
     }
-
-    set(key,value) {
-        let index = this._hash(key);
-        if(!this.dataMap[index]) this.dataMap[index] = [];
-        
-        this.dataMap[index].push([key, value]);
-        return this;
-    }
-
-    get(key) {
-        let index = this._hash(key);
-        if(this.dataMap[index]) {
-            for(let i = 0; i < this.dataMap[index].length; i++) {
-                if(this.dataMap[index][i][0] === key) {
-                    return this.dataMap[index][i][1];
-                }
-            }
-        }
-        return undefined;
-    }
-
-    keys() {
-        let allKeys = [];
-        for(let i = 0; i < this.dataMap.length; i++) {
-            if(this.dataMap[i]) {
-                for(let j = 0; j < this.dataMap[i].length; j++) {
-                    allKeys.push(this.dataMap[i][j][0]);
-                }
-            }
-        }
-        return allKeys;
-    }
-
+    return allKeys;
+  }
 }
-
-
 
 function test() {
+  let myHashTable = new HashTable();
 
-    let myHashTable = new HashTable();
+  myHashTable.set("paint", 20);
+  myHashTable.set("bolts", 40);
+  myHashTable.set("nails", 100);
+  myHashTable.set("tile", 50);
+  myHashTable.set("lumber", 80);
 
-    myHashTable.set("paint", 20);
-    myHashTable.set("bolts", 40);
-    myHashTable.set("nails", 100);
-    myHashTable.set("tile", 50);
-    myHashTable.set("lumber", 80);
-
-    console.log( myHashTable.keys() );
+  console.log(myHashTable.keys());
 }
 
-
 test();
-
 
 /*
     EXPECTED OUTPUT:
     ----------------
     [ 'paint', 'bolts', 'nails', 'tile', 'lumber' ]
 
-*/   
+*/
